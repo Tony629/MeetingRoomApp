a568d33f03d40e4f9578c0db6d60340c77eb0fe5
Tony629 <luck_successful@163.com>
2025-01-27 10:53:44 +0800

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
 import { Navigate, useLocation } from "react-router-dom";
+import Cookies from 'js-cookie';
 import { useAuth } from "../context/AuthContext";
 
-
 export default function PrivateRoute({ children }: any) {
-    const { isLoggedIn } = useAuth();
+    //const { isLoggedIn } = useAuth(); 浏览器刷新后会导致重新登录
     const location = useLocation();
+    const isAuthenticated = Cookies.get('isAuthenticated');
 
-    console.log("isLoggedIn:" + isLoggedIn);
+    console.log("isAuthenticated:" + isAuthenticated);
 
-    if (!isLoggedIn) {
+    if (!isAuthenticated) {
         return <Navigate to="/login" state={{ from: location }} replace />
     }
 
